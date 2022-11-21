e5db40acab766933c17f89a780a0e1bcd5dfb0b7
saraalvarex <alvarezalmacen19@gmail.com>
2022-11-21 14:25:53 +0100

    SYNCS

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute} from '@angular/router';
+import { CubosService } from 'src/app/services/cubos.service';
+import { Cubo } from './../../models/cubo';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public cubos!: Array<Cubo>;
+  constructor(private _service: CubosService, private _activeRoutes: ActivatedRoute) { }
+  // comprar(){
+  //   this._service.realizarCompra(id).subscribe(res=>{
 
-  constructor() { }
-
+  //   }
+  // }
   ngOnInit(): void {
+        this._service.getCubos().subscribe(res=>{
+          this.cubos = res;
+        })
   }
 
 }
